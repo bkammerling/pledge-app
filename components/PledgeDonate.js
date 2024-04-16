f87d0dbb60a12959c3ab6781b0cec2934ffcb24c
Ben Kammerling <bkammerling@googlemail.com>
2024-04-16 15:39:26 +0100

    Updated to new funraise form

--- components/PledgeDonate.js
+++ components/PledgeDonate.js
@@ -39,7 +39,7 @@ const PledgeDonate = ({ innerRef, afterDonate, blok, name }) => {
   }
 
   useEffect(() => {
-    window.funraise.push('onSuccess', { form: 18354 }, donationSuccess);
+    window.funraise.push('onSuccess', { form: 36838 }, donationSuccess);
   }, []);
 
   const asks = blok.buttons?.map((nestedButton, index) => ( Number(nestedButton.amount) )) || [5,10,20,50];
@@ -51,8 +51,8 @@ const PledgeDonate = ({ innerRef, afterDonate, blok, name }) => {
         {`(function(f,u,n,r,a,i,s,e){var data={window:window,document:document,tag:"script",data:"funraise",orgId:f,uri:u,common:n,client:r,script:a};var scripts;var funraiseScript;data.window[data.data]=data.window[data.data]||[];if(data.window[data.data].scriptIsLoading||data.window[data.data].scriptIsLoaded)return;data.window[data.data].loading=true;data.window[data.data].push("init",data);scripts=data.document.getElementsByTagName(data.tag)[0];funraiseScript=data.document.createElement(data.tag);funraiseScript.async=true;funraiseScript.src=data.uri+data.common+data.script+"?orgId="+data.orgId;scripts.parentNode.insertBefore(funraiseScript,scripts)})('339fd70c-b9c5-4839-bb00-d05ef087d209','https://assets.funraise.io','/widget/common/2.0','/widget/client','/inject-form.js');`}
       </Script>
       <Script id="funraise-form">
-        {`window.funraise.push('create', { form: 18354 });
-        window.funraise.push('config', { form: 18354 }, {
+        {`window.funraise.push('create', { form: 36838 });
+        window.funraise.push('config', { form: 36838 }, {
           defaultValues: {
             ask: '${askString}',
           },
@@ -74,7 +74,7 @@ const PledgeDonate = ({ innerRef, afterDonate, blok, name }) => {
                   <div className="col-sm-8 col-md-6">                    
                     <div className="d-grid gap-3">
                       {blok.buttons && blok.buttons.map((nestedButton, index) => (
-                        <button {...storyblokEditable(nestedButton)} id={index==1 ? 'first-donate-button' : null} type="button" className="btn btn-primary fw-semibold" data-formid="18354" data-amount={nestedButton.amount} data-frequency={nestedButton.recurring ? 'm' : 'o'}>{nestedButton.text}</button>
+                        <button {...storyblokEditable(nestedButton)} id={index==1 ? 'first-donate-button' : null} type="button" className="btn btn-primary fw-semibold" data-formid="36838" data-amount={nestedButton.amount} data-frequency={nestedButton.recurring ? 'm' : 'o'}>{nestedButton.text}</button>
                       ))}
                       <button id="skip-donation-button" type="button" onClick={afterDonate} className="btn text-dark btn-link">No thanks, I'll skip</button>
                     </div>
